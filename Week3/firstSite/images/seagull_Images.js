e3563e298c34b28c2018212e8d8ce2982e71c0dd
Enzo Di Paola <dipaolae98@gmail.com>
2019-11-04 14:29:05 +0000

    adding file

---
+++ Week3/firstSite/images/seagull_Images.js
@@ -0,0 +1,7 @@
+let seagullImages = [
+    "https://s3.amazonaws.com/aphs.worldnomads.com/locomocean/27253/IMG_2583.jpg",
+    "http://1.bp.blogspot.com/-TgjMIYvsdTU/TqZL1CQhYCI/AAAAAAAACgo/Jl9LM9iuUUA/s1600/Cute-Seagull-image-01.jpg",
+    "https://i.pinimg.com/originals/01/f8/cf/01f8cf2496a972b9c7d52adeb7750e4d.jpg",
+    "https://i.pinimg.com/originals/e4/36/bd/e436bdd6a56de951ab9ed670b18d77fe.jpg",
+    "https://i2.nicepik.com/files/972/600/846/seagull-look-stare-bird.jpg",
+];
--- Week3/firstSite/js/script.js
+++ Week3/firstSite/js/script.js
@@ -1,3 +1,11 @@
-document.getElementById("image").addEventListener("click", ()=>{
-    document.getElementById("image").src = "images/angry.jpg";
-})
+
+for(i=0;i<seagullImages.length;i++){
+    document.getElementsByTagName("body")[0].innerHTML += "<img id='image' src=" + seagullImages[i] + ">";
+}
+
+let images = document.getElementsByTagName("img");
+for(i=0;i<images.length;i++){
+    images[i].addEventListener("click", (event)=>{
+        event.target.src = "images/angry.jpg";
+    })
+}
